5ce2915d6eb18c974e840bff7f3c62012ba30b36
Md. Porag Sarkar <porag-m06>
2023-08-31 16:29:56 +0600

    update modifier

--- person.rb
+++ person.rb
@@ -17,14 +17,14 @@ class Person < Nameable
     of_age? || @parent_permission
   end
 
+  # Implementing the correct_name method that returns the name
+  def correct_name
+    @name
+  end
+
   private
 
   def of_age?
     @age >= 18
-  end
-
-  # Implementing the correct_name method that returns the name
-  def correct_name
-    @name
   end
 end
