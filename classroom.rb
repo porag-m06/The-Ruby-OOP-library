782bd1a3a2f0d6bc0fc657f1cf0f7eb2c350bcb4
Md. Porag Sarkar <porag-m06>
2023-09-01 21:14:58 +0600

    classroom class created & Student class modified to sort associations

---
+++ classroom.rb
@@ -0,0 +1,13 @@
+# Classroom is class with the only attribute label
+class Classroom
+  attr_accessor :label, :students
+
+  def initialize(label)
+    @label = label
+    @students = []
+  end
+
+  def add_student(student)
+    @students << student
+    student.classroom = self
+end
--- student.rb
+++ student.rb
@@ -1,16 +1,22 @@
 require_relative 'person'
+require_relative 'classroom'
 
 # The Student class represents a student with an id, name, age, and classroom.
 # It inherits from the Person class and includes an additional method to play hooky.
 class Student < Person
-  attr_accessor :classroom
+  attr_reader :classroom
 
-  def initialize(age, classroom, name = 'Unknown', parent_permission: true)
+  def initialize(age, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission)
-    @classroom = classroom
+    @classroom = nil
   end
 
   def play_hooky
     '¯(ツ)/¯'
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students << self unless classroom.students.include?(self)
   end
 end
