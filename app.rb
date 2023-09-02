1f3e1469abae5153f33573b5c0b2354795c8edaf
Md. Porag Sarkar <porag-m06>
2023-09-03 00:33:10 +0600

    update

--- app.rb
+++ app.rb
@@ -11,6 +11,21 @@ class App
     @people = []
   end
 
+  def app_instruction
+    puts ''
+    puts 'Welcome to School Library App!'
+    puts ''
+    puts 'Please choose an option by entering a number:'
+    puts '1 - List all books'
+    puts '2 - List all people'
+    puts '3 - Create a person'
+    puts '4 - Create a book'
+    puts '5 - Create a rental'
+    puts '6 - List all rentals for a given person id'
+    puts '7 - Exit'
+    puts ''
+  end
+
   def list_all_books
     @books.each do |book|
       puts "Title: #{book.title}, Author: #{book.author}"
--- main.rb
+++ main.rb
@@ -7,19 +7,7 @@ class Main
   end
 
   def run
-    puts ''
-    puts 'Welcome to School Library App!'
-    puts ''
-    puts 'Please choose an option by entering a number:'
-    puts '1 - List all books'
-    puts '2 - List all people'
-    puts '3 - Create a person'
-    puts '4 - Create a book'
-    puts '5 - Create a rental'
-    puts '6 - List all rentals for a given person id'
-    puts '7 - Exit'
-    puts ''
-
+    @app.app_instruction
     option = gets.chomp.to_i
 
     case option
