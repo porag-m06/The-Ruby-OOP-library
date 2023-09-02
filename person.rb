172f15b1c79b44c7ff0f5261616d6bf08cbdb4ec
Md. Porag Sarkar <porag-m06>
2023-09-03 01:36:26 +0600

    final linter fix

--- person.rb
+++ person.rb
@@ -6,7 +6,7 @@ class Person < Nameable
   attr_reader :id, :rentals
   attr_accessor :name, :age
 
-  def initialize(age, name = 'Unknown', parent_permission = true)
+  def initialize(age, name = 'Unknown', parent_permission: true)
     super()
     @id = Random.rand(1..200)
     @name = name
