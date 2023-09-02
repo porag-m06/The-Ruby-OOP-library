efb6817ab29e518ef1d77abb4e0180d5b4656d86
Md. Porag Sarkar <porag-m06>
2023-09-02 17:40:25 +0600

    demo console app setup

--- app.rb
+++ app.rb
@@ -1,1 +1,17 @@
 # Console App entry-point
+class App
+  def initialize
+    puts "Console App initialize..."
+  end
+  
+  def run
+    puts "Running console app now..."
+  end
+end
+
+def main
+  app = App.new()
+  app.run()
+end
+
+main()
